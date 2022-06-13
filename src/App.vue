8258c32f7f9a4de504c7becdec6525b2cf97323b
lenovo <radheshyamb9@gmail.com>
2022-06-13 13:32:49 +0530

    ADDED : use Methods add Comment

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,28 @@
     <p>Pixels across: {{ x }}</p>
   </div>
 </div>
+<div id="app">
+  <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/vue-post-photo.jpg" class="main-photo">
+  <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/vue-main-profile.jpg" class="main-profile">
+  <div class="main-info">
+    <span class="name">Julianne Delfina</span> 
+    <h3>"It's lovely after it rains"</h3>
+  </div>
+  <hr>
+  
+  <ul>
+    <li v-for="comment in comments" :key="comment">
+      {{ comment }}
+    </li>
+  </ul>
+  
+  <input
+    @keyup.enter="addComment"
+    v-model="newComment"
+    placeholder="Add a comment"
+  />
+</div>
+
 </template>
 
 <script>
@@ -31,6 +53,22 @@ export default {
 	  },
     xCoordinate(e) {
       this.x = e.clientX;
+    }
+  },
+   data() {
+    return {
+      newComment: '',
+      comments: [
+        'Looks great Julianne!',
+        'I love the sea',
+        'Where are you at?'
+      ]
+    }
+  },
+  methods: {
+    addComment() {
+      this.comments.push(this.newComment)
+      this.newComment = ''
     }
   }
 }
