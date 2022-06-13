463fbf2e4bef61d48266e6ec5743fe73051407c5
lenovo <radheshyamb9@gmail.com>
2022-06-13 13:04:49 +0530

    ADDED : Use Methods and Events

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app" >
+  <div @mousemove="xCoordinate" :style="{ backgroundColor: `hsl(${x}, 80%, 50%)` }" class="box">
+    <p>
+      <button v-on:click="increment(5)">+ 5</button> 
+      {{ counter }} 
+      <button @click="decrement(5)">- 5</button>
+    </p>
+    <p>Pixels across: {{ x }}</p>
+  </div>
+</div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  data() {
+  	return {
+  		counter: 0,
+      x: 0
+  	}
+  },
+  methods: {
+	  increment(num) {
+	  	this.counter+= num;
+	  },
+    decrement(num) {
+	  	this.counter-= num;
+	  },
+    xCoordinate(e) {
+      this.x = e.clientX;
+    }
+  }
+}
+</script>
